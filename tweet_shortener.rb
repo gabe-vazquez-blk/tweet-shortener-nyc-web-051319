94f574cf18690d939f4e38691054419b2086cf36
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-06 23:31:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,9 +30,6 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-def selective_tweet_shortener (tweets)
-  tweets.each do |tweet|
-    puts word_substituter(tweet) if tweet.length > 140
-    puts tweet
-  end
+def selective_tweet_shortener (tweet)
+
 end  
