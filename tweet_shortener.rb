e60d241d0af4e164a6a38bb5341333416a0d4a05
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-06 23:16:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -22,5 +22,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortner(tweets)
-  tweets.map do |tweet|
-    word_substituter(tweet)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
