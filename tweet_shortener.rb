64101dd500f01fdbf44f956463ccbbb52e1b7bff
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-06 23:51:36 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,6 +5,9 @@ def dictionary
     "to" => "2",
     "two" => "2",
     "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "For" =>"4",
     "be" => "b",
     "you" => "u",
     "at" => "@",
@@ -12,6 +15,42 @@ def dictionary
   }
 end
 
-def word_substitute(tweet)
-  dictionary["hello"]
+def word_substituter(tweet)
+  dictionary = dictionary()
+  tweet = tweet.split(" ")
+  tweet.map do |w|
+    w = dictionary[w] if dictionary.keys.include?(w)
+    w
+  end.join(" ")
 end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener (tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end  
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    new_tweet = word_substituter(tweet)
+    if new_tweet.length > 140
+      new_tweet[0..136] + "..."
+    end
+  else
+    tweet
+  end
+end
+
+
+
+
+
+
