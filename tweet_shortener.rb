41b6ad7fe078e56300e1bc1ba17890ae042b2ab8
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-06 22:39:13 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substitute(tweet)
+  dictionary["hello"]
+end
